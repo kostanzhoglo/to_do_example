6e69b30967fac18511e82958927439af198e01d9
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-28 12:45:26 -0500

    Add 2 tests to input-form.spec, 1 to ensure input is focused, 1 to ensure input can accept input-value

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,6 +4,12 @@ export default props =>
   <form>
     <input
       type='text'
+      autoFocus
       className="new-todo"
       placeholder="What needs to be done?"/>
+    <input
+      type='text'
+      // autoFocus
+      className="my-dad"
+      placeholder="Whatchoo doin'?"/>
   </form>
