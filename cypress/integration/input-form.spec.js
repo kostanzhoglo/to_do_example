fd6982e2dd1a37fdd97b909cefaeda6eb221a0f8
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-28 16:40:28 -0500

    Add test to make sure error response is correct for failed API call. Add error handling to TodoApp so that this test will pass.

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -17,7 +17,7 @@ describe('Input form', () => {
   })
 
   context('Form submission', () => {
-    it.only('Adds a new todo on submit', () => {
+    it('Adds a new todo on submit', () => {
       const itemText = 'Buy eggs'
       cy.server() // starts up the Cypress server, so you can stub responses with known data, allowing you to test functionality even if the back-end isn't built yet.
       // cy.route DEFINES the Request you want to handle. The 3rd parameter is a predefined response you want to SEND Back to your app, as if you had successfully reached a real, working API.
@@ -27,12 +27,30 @@ describe('Input form', () => {
         isComplete: false
       })
       cy.get('.new-todo')
-        .type(itemText)
+        .type(itemText)  // hmmmmm
         .type('{enter}')
         .should('have.value', '')
       cy.get('.todo-list li')
         .should('have.length', 1)
         .and('contain', itemText)
+    })
+    it('Shows an error message on a failed submission', () => {
+      cy.server()
+      cy.route({
+        url: '/api/todos',
+        method: 'POST',
+        status: 500,
+        response: {}
+      })
+
+      cy.get('.new-todo')
+        .type('test{enter}')
+
+      cy.get('.todo-list li')
+        .should('not.exist')
+
+      cy.get('.error')
+        .should('be.visible')
     })
   })
 })
