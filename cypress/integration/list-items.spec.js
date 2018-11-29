ce021c4aacb85c63d564a12e1af20a79a4514e83
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-29 15:10:03 -0500

    Finish test about todo deletion

--- cypress/integration/list-items.spec.js
+++ cypress/integration/list-items.spec.js
@@ -12,8 +12,31 @@ describe('List-items', () => {
       .should('be.checked')
   })
 
-  it.only('Shows remaining todos in the footer', () => {
+  it('Shows remaining todos in the footer', () => {
     cy.get('.todo-count')
       .should('contain', 3)
+  })
+
+  it.only('Removes a todo', () => {
+    cy.route({
+      url: '/api/todos/1',
+      method: 'DELETE',
+      status: 200,
+      response: {}
+    })
+
+    cy.get('.todo-list li')
+      .as('list')  // can now grab the line above as alias:  <<<  @list  >>>
+
+    cy.get('@list') // alias as mentioned above!
+      .first() // grabs FIRST list item in .todo-list li array.
+      .find('.destroy')
+      .invoke('show') // makes Delete button visible, by SHOWing it. Once it's SHOWn, now Cypress tests can interact with it and 'click' it.
+      .click()
+      // .click({force: true})   // this is the NOT preferred way of forcing a hidden element to appear because it disables Cypress' internal checks.  Better to use .invoke('show') method above instead.
+
+    cy.get('@list')
+      .should('have.length', 3)
+      .and('not.contain', 'Milk')
   })
 })
