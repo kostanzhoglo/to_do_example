8700d009852463ea42b0d4455a2920576198052a
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-28 21:20:12 -0500

    Add code to make first test pass in app-init.spec. loadTodos method in lib/service.js, then import that in to TodoApp.js

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -24,7 +24,7 @@ describe('App initialization', () => {
   it.only('Loads todos on page load', () => {
     cy.server()
     // 3rd argument is the RESPONSE BODY from the API call, and we want to respond with an array of todos.
-    cy.route('GET', '/apis/todos', todos)
+    cy.route('GET', '/api/todos', todos)
     cy.visit('/')
 
     cy.get('.todo-list li')
--- src/lib/service.js
+++ src/lib/service.js
@@ -4,3 +4,6 @@ export const saveTodo = (todo) =>
   axios.post('http://localhost:3030/api/todos', todo)
   // returns {data} from the api (eventually)
   // that {data} Object will be used in TodoApp.js, handleTodoSubmit()...
+
+export const loadTodos = () =>
+  axios.get('http://localhost:3030/api/todos')
