4a70658c97e5a7bb9155f053141d743e624aeb41
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-28 21:29:52 -0500

    Create fixtures/todos.json, pull todos array in there, and out of app-init.spec file

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,30 +1,8 @@
-const todos = [
-  {
-    "id": 1,
-    "name": "Buy Milk",
-    "isComplete": false
-  },
-  {
-    "id": 2,
-    "name": "Buy Eggs",
-    "isComplete": false
-  },
-  {
-    "id": 3,
-    "name": "Buy Bread",
-    "isComplete": false
-  },
-  {
-    "id": 4,
-    "name": "Make French Toast",
-    "isComplete": false
-  }
-]
 describe('App initialization', () => {
   it.only('Loads todos on page load', () => {
     cy.server()
     // 3rd argument is the RESPONSE BODY from the API call, and we want to respond with an array of todos.
-    cy.route('GET', '/api/todos', todos)
+    cy.route('GET', '/api/todos', 'fixture:todos')
     cy.visit('/')
 
     cy.get('.todo-list li')
