12c99d905f1a13864c5fc10b8b8e8f444af2c557
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-29 21:41:07 -0500

    Refactor link filter test for footer, to keep code DRY

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -12,17 +12,34 @@ describe('Footer', () => {
       cy.seedAndVisit()
     })
 
-    it('display plural todos in count', () => {
+    it('displays plural todos in count', () => {
       cy.get('.todo-count')
         .should('contain', '3 todos left')
     })
 
-    it.only('Filters to active todos', () => {
-      cy.contains('Active')  // find the link you want based on its CONTEN (text)
-        .click()
+    // IF I was only testing for 1 filter option. Below is code to check ALL 3 filter options at same time.  Refactor and DRY.
+    // it('Filters to active todos', () => {
+    //   cy.contains('Active')  // find the link you want based on its CONTENT (text)
+    //     .click()
+    //
+    //   cy.get('.todo-list li')
+    //     .should('have.length', 3)
+    // })
 
-      cy.get('.todo-list li')
-        .should('have.length', 3)
+    it.only('Handles filter links', () => {
+      const filters = [
+        {link: 'Active', expectedLength: 3},
+        {link: 'Completed', expectedLength: 1},
+        {link: 'All', expectedLength: 4}
+      ]
+      cy.wrap(filters)  // this puts filters in the context of Cypress.  This now allows us to chain on additional Cypress commands.
+        .each(filter => {
+          cy.contains(filter.link)
+            .click()
+
+          cy.get('.todo-list li')
+            .should('have.length', filter.expectedLength)
+        })
     })
   })
 })
